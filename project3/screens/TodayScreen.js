789fa6630f628fff5cac73bd9643e7aea0bb0377
Gustav Dyngeseth <gustdyn@gmail.com>
2018-09-28 02:35:54 +0200

    Add registration of user data and initiate AsyncStorage #5

--- project3/screens/TodayScreen.js
+++ project3/screens/TodayScreen.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import Expo from "expo";
-
+ 
 import { View, Text, AsyncStorage, StyleSheet, Button } from 'react-native';
-
-
+ 
+ 
 export default class TodayScreen extends React.Component {
   static navigationOptions = {
     title: 'Today',
   };
-
-
+ 
+ 
   //Function that removes user from asyncStorage and navigates back to registerScreen.
   _signOutAsync = async () => {
-    await AsyncStorage.removeItem('userToken');
+    //This is how to access user data, (result) is now a object with all the user attributes.
+    await AsyncStorage.getItem('USER', (err, result) => {
+      console.log(result);
+    });
+ 
+    //Still keeping this to make it possible to navigate between register and app.
+    await AsyncStorage.removeItem('USER');
     this.props.navigation.navigate('Auth');
   }
-
+ 
   render() {
     return (
       <View style={styles.container}>
@@ -25,7 +31,7 @@ export default class TodayScreen extends React.Component {
     );
   }
 }
-
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -34,9 +40,8 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
 });
-
+ 
 Expo.registerRootComponent(TodayScreen);
-
 
  
 

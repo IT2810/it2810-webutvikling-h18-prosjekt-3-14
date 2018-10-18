babdad8ed8fd92a832a1fcf5249b5c8d8b28bbdc
Unknown <kiddid96@gmail.com>
2018-10-18 02:50:01 +0200

    Update tests to match new codestyle #4

--- project3/components/__tests__/Helpers.test.js
+++ project3/components/__tests__/Helpers.test.js
@@ -2,106 +2,88 @@ import Helpers from "./../Helpers"
 
 
 // Calculate BMI
-test('_calculateBMI: Check general BMI example. Calculated using formula for BMI', () => {
-    expect(Helpers._calculateBMI(80, 180)).toBe(24.691358024691358);
+test('calculateBMI: Check general BMI example. Calculated using formula for BMI', () => {
+    expect(Helpers.calculateBMI(80, 180)).toBe(24.691358024691358);
 });
 
-test('_calculateBMI: Check BMI with zero input', () => {
-    expect(()=>{Helpers._calculateBMI(0,0)}).toThrow(RangeError);
+test('calculateBMI: Check BMI with zero input', () => {
+    expect(()=>{Helpers.calculateBMI(0,0)}).toThrow(RangeError);
 });
 
-test('_calculateBMI: Check BMI with not number input', () => {
-    expect(()=>Helpers._calculateBMI("hello", "there")).toThrowError(TypeError);
+test('calculateBMI: Check BMI with not number input', () => {
+    expect(()=>Helpers.calculateBMI("hello", "there")).toThrowError(TypeError);
 });
 
-test('_calculateBMI: Check BMI with not null  input', () => {
-    expect(()=>Helpers._calculateBMI(null, null)).toThrow(TypeError);
+test('calculateBMI: Check BMI with not null  input', () => {
+    expect(()=>Helpers.calculateBMI(null, null)).toThrow(TypeError);
 });
 
 // Add space between numbers
-test('_addSpaceBetweenNumber: Check general example', () => {
-    expect(Helpers._addSpaceBetweenNumber(1000)).toBe("1 000");
+test('addSpaceBetweenNumber: Check general example', () => {
+    expect(Helpers.addSpaceBetweenNumber(1000)).toBe("1 000");
 });
 
-test('_addSpaceBetweenNumber: Check null input', () => {
-    expect(Helpers._addSpaceBetweenNumber(null)).toBe(NaN);
+test('addSpaceBetweenNumber: Check null input', () => {
+    expect(Helpers.addSpaceBetweenNumber(null)).toBe(NaN);
 });
 
-test('_addSpaceBetweenNumber: Check not number', () => {
-    expect(Helpers._addSpaceBetweenNumber("hello")).toBe(NaN);
+test('addSpaceBetweenNumber: Check not number', () => {
+    expect(Helpers.addSpaceBetweenNumber("general")).toBe(NaN);
 });
 
 
 // Calculate calories
-test('_calculateCaloriesBurned: Check general example', () => {
-    expect(Helpers._calculateCaloriesBurned(80, 180, 10000)).toBe(389);
+test('calculateCaloriesBurned: Check general example', () => {
+    expect(Helpers.calculateCaloriesBurned(80, 180, 10000)).toBe(389);
 });
 
-test('_calculateCaloriesBurned: Check for 0', () => {
-    expect(Helpers._calculateCaloriesBurned(0, 0, 0)).toBe(NaN);
+test('calculateCaloriesBurned: Check for 0', () => {
+    expect(Helpers.calculateCaloriesBurned(0, 0, 0)).toBe(0);
 });
 
-test('_calculateCaloriesBurned: Check for negative numbers', () => {
-    expect(()=>Helpers._calculateCaloriesBurned(-1, -2, -3)).toThrow(RangeError);
+test('calculateCaloriesBurned: Check for negative numbers', () => {
+    expect(()=>Helpers.calculateCaloriesBurned(-1, -2, -3)).toThrow(RangeError);
 });
 
-test('_calculateCaloriesBurned: Check not number', () => {
-    expect(()=>Helpers._calculateCaloriesBurned("Hello", ["there"], null)).toThrow(TypeError);
+test('calculateCaloriesBurned: Check not number', () => {
+    expect(()=>Helpers.calculateCaloriesBurned("kenobi", [""], null)).toThrow(TypeError);
 });
 
 // Calculate distance
-test('_calculateDistance: Check general example', () => {
-    expect(Helpers._calculateDistance(183, 1000)).toBe(757.62); //TODO: Find actual example
+test('calculateDistance: Check general example', () => {
+    expect(Helpers.calculateDistance(183, 1000)).toBe(757.62); //TODO: Find actual example
 });
 
-test('_calculateDistance: Check for 0', () => {
-    expect(Helpers._calculateDistance(0, 0)).toBe(0);
+test('calculateDistance: Check for 0', () => {
+    expect(Helpers.calculateDistance(0, 0)).toBe(0);
 });
 
-test('_calculateDistance: Check for negative numbers', () => {
-    expect(()=>Helpers._calculateDistance(-1, -2)).toThrow(RangeError);
+test('calculateDistance: Check for negative numbers', () => {
+    expect(()=>Helpers.calculateDistance(-1, -2)).toThrow(RangeError);
 });
 
-test('_calculateDistance: Check for not number', () => {
-    expect(()=>Helpers._calculateDistance("hello", ["there"])).toThrow(TypeError);
+test('calculateDistance: Check for not number', () => {
+    expect(()=>Helpers.calculateDistance("ur", ["mom"])).toThrow(TypeError);
 });
 
 // Calculate goal progress
-test('_calculateGoalProgress: Check general example', () => {
-    expect(Helpers._calculateGoalProgress(10, 100)).toBe(10);
+test('calculateGoalProgress: Check general example', () => {
+    expect(Helpers.calculateGoalProgress(10, 100)).toBe(10);
 });
 
-test('_calculateGoalProgress: Check 0', () => {
-    expect(()=>Helpers._calculateGoalProgress(0, 0)).toThrow(RangeError);
+test('calculateGoalProgress: Check 0', () => {
+    expect(()=>Helpers.calculateGoalProgress(0, 0)).toThrow(RangeError);
 });
 
-test('_calculateGoalProgress: Check negative', () => {
-    expect(()=>Helpers._calculateGoalProgress(-2, -3)).toThrow(RangeError);
+test('calculateGoalProgress: Check negative', () => {
+    expect(()=>Helpers.calculateGoalProgress(-2, -3)).toThrow(RangeError);
 });
 
-test('_calculateGoalProgress: Check not number', () => {
-    expect(()=>Helpers._calculateGoalProgress([2], "hello")).toThrow(TypeError);
+test('calculateGoalProgress: Check not number', () => {
+    expect(()=>Helpers.calculateGoalProgress([2], "hello")).toThrow(TypeError);
 });
 
-// As the _calculateGoalProgress function is used for calculating the percentage to fill the circular progress bar, all current > goal => 100
-test('_calculateGoalProgress: Check current > goal', () => {
-    expect(Helpers._calculateGoalProgress(1000, 100)).toBe(100);
+// As the calculateGoalProgress function is used for calculating the percentage to fill the circular progress bar, all current > goal => 100
+test('calculateGoalProgress: Check current > goal', () => {
+    expect(Helpers.calculateGoalProgress(1000, 100)).toBe(100);
 });
-
-
-
-
-
-// BMI Stage
-
-
-
-
-
-
-
-
-
-
-
-

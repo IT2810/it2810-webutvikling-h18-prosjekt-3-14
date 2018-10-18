0c9d2666f451bda3f765d5e8bd17185aa5198d86
Unknown <kiddid96@gmail.com>
2018-10-18 19:26:43 +0200

    Move helpers to utils #4

--- project3/utils/Helpers.js
+++ project3/utils/Helpers.js
@@ -1,7 +1,6 @@
-import Expo from 'expo'
+import {Pedometer} from "expo";
 
-export default class Helpers {
-
+const helpers = {
   /**
    * Gets the calories burned based on a formula found online
    * @param weight Users weight
@@ -9,7 +8,7 @@ export default class Helpers {
    * @param steps Number of steps taken today
    * @returns {number}
    */
-  static calculateCaloriesBurned(weight, height, steps) {
+  calculateCaloriesBurned: (weight, height, steps) => {
     if (typeof weight !== typeof 0 || typeof height !== typeof 0 || typeof steps !== typeof 0) {
       throw new TypeError("Input not numbers");
     }
@@ -17,11 +16,11 @@ export default class Helpers {
       throw new RangeError("Input out of range");
     }
 
-    const distance = parseFloat(Helpers.calculateDistance(height, steps));
+    const distance = parseFloat(helpers.calculateDistance(height, steps));
     const time = distance / 1.34112 > 0 ? distance / 1.34112 : 1;   // Guaranteed no zero division
     const met = (distance / time * 2.23693629);
     return Math.round(met * 3.5 * (weight) / 200 * (time / 60)); // Formula found online
-  };
+  },
 
   /**
    * Formats numbers to have spaces for every third number. 1000 -> 1 000.
@@ -29,13 +28,13 @@ export default class Helpers {
    * @param number
    * @returns {String}
    */
-  static addSpaceBetweenNumber(number) {
-    if (typeof number !== typeof 0) {
+  addSpaceBetweenNumber: (number) => {
+    if (typeof number !== typeof 0)
       return NaN;
-    }
+    
 
     return number != null ? number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") : null;
-  };
+  },
 
   /**
    * Calculates BMI with formula: weight/(height)²
@@ -43,17 +42,17 @@ export default class Helpers {
    * @param height Height in CM
    * @returns {number}
    */
-  static calculateBMI(weight, height) {
-    if (typeof weight !== typeof 0 || typeof height !== typeof 0) {
+  calculateBMI: (weight, height) => {
+    if (typeof weight !== typeof 0 || typeof height !== typeof 0)
       throw new TypeError("Input not numbers");
-    }
-    if (weight < 0 || height <= 0) {
+
+    if (weight < 0 || height <= 0)
       throw new RangeError("Input out of range");
-    }
+
 
     height = height === 0 ? 1 : height;
     return weight / ((height / 100)) ** 2;
-  };
+  },
 
   /**
    * Calculate distance walked based on users step distance which is estimated from users height
@@ -61,17 +60,16 @@ export default class Helpers {
    * @param steps Number of steps taken
    * @returns {number}
    */
-  static calculateDistance(height, steps) {
-    if (typeof height !== typeof 0 || typeof steps !== typeof 0) {
+  calculateDistance: (height, steps) => {
+    if (typeof height !== typeof 0 || typeof steps !== typeof 0)
       throw new TypeError("Input not numbers");
-    }
-    if (height < 0 || steps < 0) {
-      throw new RangeError("Input out of range");
-    }
 
-    let sd = (height * 0.414) / 100; // step distance. Formula found online
+    if (height < 0 || steps < 0)
+      throw new RangeError("Input out of range");
+
+    let sd = (height * 0.45) / 100; // step distance. Formula found online
     return steps * sd;
-  };
+  },
 
   /**
    * Calculates the percentage done on a goal, outputs 100 if current > goal
@@ -79,15 +77,24 @@ export default class Helpers {
    * @param goal
    * @returns {number}
    */
-  static calculateGoalProgress(current, goal) {
-    if (typeof current !== typeof 0 || typeof goal !== typeof 0) {
+  calculateGoalProgress: (current, goal) => {
+    if (typeof current !== typeof 0 || typeof goal !== typeof 0)
       throw new TypeError("Input not numbers");
-    }
-    if (current < 0 || goal <= 0) {
-      throw new RangeError("Goal cannot be 0 or lower");
-    }
-    return current / goal <= 1 ? (current / goal) * 100 : 100;
-  };
-}
 
-Expo.registerRootComponent(Helpers);
+    if (current < 0 || goal <= 0)
+      throw new RangeError("Goal cannot be 0 or lower");
+
+    return current / goal <= 1 ? (current / goal) * 100 : 100;
+  },
+
+  getStepCountBetweenDates: async (start=new Date(), end=new Date()) => {
+    start.setHours(0, 0, 0, 0);   // Set time so that you get all steps from 00:00 today
+
+    return await Pedometer.getStepCountAsync(start, end)
+      .then(
+        (result) => result.steps,
+        (error) => {throw new Error(error)});
+  },
+};
+
+export default helpers
--- project3/utils/__tests__/Helpers.test.js
+++ project3/utils/__tests__/Helpers.test.js
@@ -1,89 +1,89 @@
-import Helpers from "./../Helpers"
+import helpers from "../Helpers"
 
 
 // Calculate BMI
 test('calculateBMI: Check general BMI example. Calculated using formula for BMI', () => {
-    expect(Helpers.calculateBMI(80, 180)).toBe(24.691358024691358);
+    expect(helpers.calculateBMI(80, 180)).toBe(24.691358024691358);
 });
 
 test('calculateBMI: Check BMI with zero input', () => {
-    expect(()=>{Helpers.calculateBMI(0,0)}).toThrow(RangeError);
+    expect(()=>{helpers.calculateBMI(0,0)}).toThrow(RangeError);
 });
 
 test('calculateBMI: Check BMI with not number input', () => {
-    expect(()=>Helpers.calculateBMI("hello", "there")).toThrowError(TypeError);
+    expect(()=>helpers.calculateBMI("hello", "there")).toThrowError(TypeError);
 });
 
 test('calculateBMI: Check BMI with not null  input', () => {
-    expect(()=>Helpers.calculateBMI(null, null)).toThrow(TypeError);
+    expect(()=>helpers.calculateBMI(null, null)).toThrow(TypeError);
 });
 
 // Add space between numbers
 test('addSpaceBetweenNumber: Check general example', () => {
-    expect(Helpers.addSpaceBetweenNumber(1000)).toBe("1 000");
+    expect(helpers.addSpaceBetweenNumber(1000)).toBe("1 000");
 });
 
 test('addSpaceBetweenNumber: Check null input', () => {
-    expect(Helpers.addSpaceBetweenNumber(null)).toBe(NaN);
+    expect(helpers.addSpaceBetweenNumber(null)).toBe(NaN);
 });
 
 test('addSpaceBetweenNumber: Check not number', () => {
-    expect(Helpers.addSpaceBetweenNumber("general")).toBe(NaN);
+    expect(helpers.addSpaceBetweenNumber("general")).toBe(NaN);
 });
 
 
 // Calculate calories
 test('calculateCaloriesBurned: Check general example', () => {
-    expect(Helpers.calculateCaloriesBurned(80, 180, 10000)).toBe(389);
+    expect(helpers.calculateCaloriesBurned(80, 180, 10000)).toBe(423);
 });
 
 test('calculateCaloriesBurned: Check for 0', () => {
-    expect(Helpers.calculateCaloriesBurned(0, 0, 0)).toBe(0);
+    expect(helpers.calculateCaloriesBurned(0, 0, 0)).toBe(0);
 });
 
 test('calculateCaloriesBurned: Check for negative numbers', () => {
-    expect(()=>Helpers.calculateCaloriesBurned(-1, -2, -3)).toThrow(RangeError);
+    expect(()=>helpers.calculateCaloriesBurned(-1, -2, -3)).toThrow(RangeError);
 });
 
 test('calculateCaloriesBurned: Check not number', () => {
-    expect(()=>Helpers.calculateCaloriesBurned("kenobi", [""], null)).toThrow(TypeError);
+    expect(()=>helpers.calculateCaloriesBurned("kenobi", [""], null)).toThrow(TypeError);
 });
 
 // Calculate distance
 test('calculateDistance: Check general example', () => {
-    expect(Helpers.calculateDistance(183, 1000)).toBe(757.62); //TODO: Find actual example
+    expect(helpers.calculateDistance(183, 1000)).toBe(823.5000000000001); //TODO: Find actual example
 });
 
 test('calculateDistance: Check for 0', () => {
-    expect(Helpers.calculateDistance(0, 0)).toBe(0);
+    expect(helpers.calculateDistance(0, 0)).toBe(0);
 });
 
 test('calculateDistance: Check for negative numbers', () => {
-    expect(()=>Helpers.calculateDistance(-1, -2)).toThrow(RangeError);
+    expect(()=>helpers.calculateDistance(-1, -2)).toThrow(RangeError);
 });
 
 test('calculateDistance: Check for not number', () => {
-    expect(()=>Helpers.calculateDistance("ur", ["mom"])).toThrow(TypeError);
+    expect(()=>helpers.calculateDistance("ur", ["mom"])).toThrow(TypeError);
 });
 
 // Calculate goal progress
 test('calculateGoalProgress: Check general example', () => {
-    expect(Helpers.calculateGoalProgress(10, 100)).toBe(10);
+    expect(helpers.calculateGoalProgress(10, 100)).toBe(10);
 });
 
 test('calculateGoalProgress: Check 0', () => {
-    expect(()=>Helpers.calculateGoalProgress(0, 0)).toThrow(RangeError);
+    expect(()=>helpers.calculateGoalProgress(0, 0)).toThrow(RangeError);
 });
 
 test('calculateGoalProgress: Check negative', () => {
-    expect(()=>Helpers.calculateGoalProgress(-2, -3)).toThrow(RangeError);
+    expect(()=>helpers.calculateGoalProgress(-2, -3)).toThrow(RangeError);
 });
 
 test('calculateGoalProgress: Check not number', () => {
-    expect(()=>Helpers.calculateGoalProgress([2], "hello")).toThrow(TypeError);
+    expect(()=>helpers.calculateGoalProgress([2], "hello")).toThrow(TypeError);
 });
 
 // As the calculateGoalProgress function is used for calculating the percentage to fill the circular progress bar, all current > goal => 100
 test('calculateGoalProgress: Check current > goal', () => {
-    expect(Helpers.calculateGoalProgress(1000, 100)).toBe(100);
+    expect(helpers.calculateGoalProgress(1000, 100)).toBe(100);
 });

16d8dda57fdfd837c62d5c89a63743b0471e4bc5
Morten Smebye <mortensmeb@gmail.com>
2018-10-19 17:50:52 +0200

    Reduce title image size #3

--- project3/components/LogoTitle.js
+++ project3/components/LogoTitle.js
@@ -2,22 +2,24 @@ import React, {Component} from 'react';
 import {Image, StyleSheet, View} from 'react-native';
 
 class LogoTitle extends Component {
-  render() {
-    return (
-      <View style={styles.imageview}>
-        <Image
-          source={require('../walktrue.jpg')}
-        />
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.imageview}>
+                <Image
+                    source={require('../walktrue.jpg')}
+                    style={{width: "70%", height: 25}}
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  imageview: {
-    left: "60%",
-    alignItems: "center"
-  }
+    imageview: {
+        width: "95%",
+        position: "absolute",
+        alignItems: "center",
+    }
 });
 
 export default LogoTitle;

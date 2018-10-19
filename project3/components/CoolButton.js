a7f7bea397a1223aa4b2f8b0b082895e18bd04e7
Unknown <kiddid96@gmail.com>
2018-10-19 18:08:40 +0200

    Add CoolButton #12

---
+++ project3/components/CoolButton.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {View, TouchableHighlight, Text, StyleSheet} from 'react-native';
+
+import layout from '../constants/Layout'
+import colors from "../constants/Colors";
+
+
+const CoolButton = ({text, onPress}) => (
+  <View>
+    <TouchableHighlight
+      style={styles.button}
+      onPress={onPress}
+      underlayColor={'fff'}
+      activeOpacity={0.5}
+    >
+      <Text>{text}</Text>
+    </TouchableHighlight>
+  </View>
+    );
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    padding: '3%',
+    margin: '3%',
+    borderWidth: 0.5,
+    borderColor: 'black',
+    color: 'red',
+    backgroundColor: 'fff',
+    width: layout.windowSize.width / 2,
+  },
+  text: {
+    width: layout.windowSize.width / 3,
+  }
+});
+
+export default CoolButton;
